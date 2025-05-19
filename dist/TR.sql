109dcfead6b8d02dc8d5ed153f702fa37bf03927
xToguia <thiagoaugusto142202@gmail.com>
2025-05-19 09:27:33 -0300

    trigger

---
+++ dist/TR.sql
@@ -0,0 +1,27 @@
+delimiter $$
+
+create trigger after_insert_line
+after insert on hopi_hari_db.line
+for each row
+begin
+
+declare wait_time int;
+declare line_count int;
+declare total_wait int;
+
+select tempo_espera into wait_time
+from atracoes 
+where atracoes_id = new.atracoes_id;
+
+select count(id) into line_count
+from hopi_hari_db.line 
+where atracoes_id = new.atracoes_id;
+
+set total_wait = wait_time * line_count;
+
+insert into notifications (description, users_id, atracoes_id, status)
+values (concat(total_wait, " MINUTOS DE ESPERA PARA O BRINQUEDO"), new.users_id, new.atracoes_id, TRUE);
+
+end$$
+
+delimiter ;
---
+++ dist/trigger.sql
@@ -0,0 +1,18 @@
+select * from users;
+
+select * from atracoes; #8
+select * from hopi_hari_db.line;
+
+insert into hopi_hari_db.line (atracoes_id, users_id) values (8, 5);
+
+select tempo_espera from atracoes where id = 8; #5 min
+select count(users_id) from hopi_hari_db.line where atracoes_id = 8; #1 pessoa
+
+select
+(select tempo_espera from atracoes where id = 8) *
+(select count(users_id) from hopi_hari_db.line where atracoes_id = 8) 
+as tempo_espera_total;
+
+desc notifications;
+insert into notifications (description ,users_id, atracoes_id, tempo_espera, status)
+values (concat(tempo_espera_total,"MINUTOS DE ESPERA PARA O BRINQUEDO"), 8, 5, TRUE);
